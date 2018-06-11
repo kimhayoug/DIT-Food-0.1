a0ac83dc2e024fd76b86f6447c44fd6c89d18952
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-06-11 16:19:14 +0900

    add

---
+++ MapTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MapTableViewController.swift
+//  UiTableViewController Test 02
+//
+//  Created by D7703_23 on 2018. 6. 11..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class MapTableViewController: UITableViewController {
+    
+    @IBOutlet weak var tatakMapView: MKPlacemark!
+    var location = [String]()
+    var names = [String]()
+    
+    var annotations = MKPointAnnotation()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        viewMap(location: location, names: names)
+    }
+    func viewMap(location:[String],names:[String]){
+        for addr in location{
+            let getCoder = CLGeocoder()
+            getCoder.geocodeAddressString(addr) {
+                (Placemarks: [CLPlacemark]?, error: Error?) -> Void in
+                if let myError = error{
+                    print(myError)
+                    return
+                }
+                if let myplacemarks = Placemarks{
+                    let mylacemarks = myplacemarks[0]
+                    let loc = myPlacemarks.location?.coordinate
+                    let anno = MKPointAnnotation()
+                    anno.coordinate = loc!
+                    anno.title = foodStoreNames[self.annotion]
+                    self.count = self.count + 1
+                    anno.subtitle = addr
+                    self.annotations.append(anno)
+                    self.myMapView.addAnnotation(self.annotion as! MKAnnotation)
+                }else{
+                    print("placemarks nil 발생")
+                }
+            }
+        }
+    }
+    }
+
+
+
